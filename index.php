2114224d40c739357c56c1e9b4de50fd0c4786fe
Samuel Gabriel <splendy5@gmail.com>
2021-06-01 18:14:43 +0800

    Updated pakages. Experimented on separete vendor package

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 if (defined('STDIN')) {chdir(dirname(__FILE__));} // Set the current directory correctly for CLI requests
 define("OAUTH_BASE", __DIR__.DIRECTORY_SEPARATOR, true);
 define("OAUTH_BASE_PATH",'system/'); //define System path
-require_once 'vendor/autoload.php'; /* Initialize composer autoloader*/
+/* Initialize composer autoloader*/
+if(boolval(getenv('SEPARATE_VENDOR'))){
+    require_once('/tmp/vendor/autoload.php');
+}
+else {
+    require_once('vendor/autoload.php');
+}
 require_once OAUTH_BASE_PATH."App.php";   
 (new App)->initialize();
