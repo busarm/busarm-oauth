b6c6f8592a7136458cc3b6b0f9491e5abfbaaff0
Samuel Gabriel <splendy5@gmail.com>
2022-01-11 23:58:23 +0800

    Add org sql insert script

---
+++ database/2_insert_orgs.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM 13243546576879_oauth.oauth_organizations
+LIMIT 0, 1000
+
+-- Date: 2022-01-11 23:52
+*/
+INSERT INTO `13243546576879_oauth`.`oauth_organizations` (`org_name`,`logo`) VALUES ('Wecari','public/images/logo/dark/logo_512px.png');
