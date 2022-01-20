d9b9aafaf655976778a37f7002dc8961afb302b4
Samuel Gabriel <splendy5@gmail.com>
2022-01-20 18:07:04 +0800

    Fix button in email template

--- application/views/email/auth.php
+++ application/views/email/auth.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Samuel
@@ -19,10 +20,12 @@ if (empty($link)) {
         </td>
     </tr>
     <tr width='350'>
-        <td  style='text-align: center;'><strong style='font-size:12px !important; color: #9d223c;'>(Please ignore this message if it wasn't triggered or requested by you)</strong></td>
+        <td style='text-align: center;'><strong style='font-size:12px !important; color: #9d223c;'>(Please ignore this
+                message if it wasn't triggered or requested by you)</strong></td>
     </tr>
     <tr width='350'>
-        <td style='text-align: center;'><span style='font-size:14px !important; color: #0b2e13;'>This link can only be used <strong>ONCE</strong> and will expire in <strong>AN HOUR</strong></span></td>
+        <td style='text-align: center;'><span style='font-size:14px !important; color: #0b2e13;'>This link can only be
+                used <strong>ONCE</strong> and will expire in <strong>AN HOUR</strong></span></td>
     </tr>
     <tr width='350'>
         <br />
@@ -30,7 +33,7 @@ if (empty($link)) {
     <tr width='350'>
         <td style='padding: 10px; display: flex; align-content: center; justify-content: center; text-align: center;'>
             <div style='margin:auto; text-align: center;'>
-                <a href='<?=$link?>' style='background-color:#3F5F44; border-radius:4px; color:#ffffff; display:inline-block; font-family:sans-serif; font-size:16px; font-weight:bold; line-height:40px; text-align:center; text-decoration:none; width:200px; -webkit-text-size-adjust:none; mso-hide:all;' rel='noreferrer'>
+                <a href="<?= $link ?>" style="margin:auto; background-color:#3F5F44; border-radius:4px; color:#ffffff; display:inline-block; font-family:sans-serif; font-size:16px; font-weight:bold; line-height:40px; text-align:center; text-decoration:none; height:40px; width:200px; -webkit-text-size-adjust:none; mso-hide:all;" rel="noreferrer">
                     Click to Login
                 </a>
             </div>
